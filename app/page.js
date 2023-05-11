f9bad7ba3eadd5bde0e7ce4c2fc39b3d252200df
messiasjs <mjls.junior.js@gmail.com>
2023-05-11 13:04:36 -0300

    test3

--- app/page.js
+++ app/page.js
@@ -64,6 +64,20 @@ function MessageTable({ products, filterText}) {
   );
 }
 
+function ProductRow({ product }) {
+  /*const name = product.stocked ? product.name :
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;*/
+
+  return (
+    <tr>
+      <td>{product.name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
 export default function Home() {
     
   /*const [blogMessages, setBlogMessages] = useState([]);
